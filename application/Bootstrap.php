3f79f9e7329bb33c092a7aa33c965de1adffc7bb
Jonathan <jramirez@creatividadeinteligencia.cl>
2012-07-06 13:07:00 -0400

    v1.0

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,42 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    protected function _initAutoLoad(){
+        /* [REGISTRO DE MODULOS] */
+        $modulos = new Zend_Application_Module_Autoloader(array(
+        'namespace'=>'',
+        'basePath'=>APPLICATION_PATH.'/modules/default'
+        ));
+        return $modulos;
+    }
+    protected function _initView()
+    {
+        $vista = new Zend_View();
+        /* [DOCTYPE] */
+        $vista->doctype('HTML5');
+        /* [META] */    
+        $vista->headMeta()
+                /* [META HTML] */
+                ->setHttpEquiv('Content-Language', 'es')
+                ->setHttpEquiv('Content-Type', 'text/html; charset=UTF-8')
+                ->appendName('author', 'Creatividad e Inteligencia')
+                ->appendName('description','')
+                ->appendName('keywords','')
+                ->appendName('robots','index,follow');
+        
+        /* [TITLE] */
+        $vista->headTitle('NOMBRE SITIO');
+        /* FAVICON */
+        $vista->headLink(array('type'=>'image/x-icon','rel' => 'shortcut icon','href' =>'/images/generales/favicon.jpg'));       
+        /* CSS */        
+        $vista->headLink()
+                ->appendStylesheet('/css/estilo.css');
+        /* [JS] */
+        $vista->headScript()
+                ->appendFile('/js/jquery.js') 
+                ->appendFile('/js/jquery.validate.js') 
+                ->appendFile('/js/jquery.sitio.js'); 
+        return $vista;
+    }   
 }
 
---
+++ application/modules/default/controllers/LayoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+class LayoutController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+    public function topAction(){}
+    public function footerAction(){}
+
+
+}
+
